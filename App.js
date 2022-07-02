b0698694024cba399c03a2c09046776fa223e23a
jumashafara <jumashafara0@gmail.com>
2022-07-02 21:46:00 +0300

    using states

--- App.js
+++ App.js
@@ -1,28 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextComponent, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextComponent, View } from "react-native";
 
 export default function App() {
+  const [person, setPerson] = useState({
+    name: "Shafara",
+    age: 22,
+    hobby: "coding",
+  });
+
+  const updatePersonInfo = () => {
+    setPerson({ name: "Juma", age: 23, hobby: "Reading" });
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.navBar}>
-        <Text style={{ fontWeight: "bold", fontFamily: "", fontSize: 20 }}>
-          <Text style={{ fontWeight: "200" }}>Data</Text> Idea
-        </Text>
-        <Text style={{ fontWeight: "bold", fontFamily: "", fontSize: 20 }}>
-          Menu
-        </Text>
-      </View>
+      <Text>My name is {person.name}</Text>
+      <Text>My age is {person.age}</Text>
+      <Text>I love {person.hobby}</Text>
       <View>
-        <View style={styles.blueContainers}>
-          <Text style={styles.heading}>HTML</Text>
-          <Text style={styles.subheading}>
-            The language for building web pages
-          </Text>
-        </View>
-        <View>
-          <Text style={styles.greenButton}>Learn HTML</Text>
-          <Text style={styles.grayButton}>HTML Reference</Text>
-        </View>
+        <Button title="Update Person Info" onPress={updatePersonInfo} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -36,43 +33,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 20,
     margin: 20,
-  },
-  navBar: {
-    display: "flex",
-    width: 400,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    padding: 20,
-    color: "#fff",
-  },
-  blueContainer: {
-    backgroundColor: "152238",
-  },
-  greenButton: {
-    backgroundColor: "#228B22",
-    padding: 10,
-    margin: 10,
-    borderRadius: 10,
-    color: "#fff",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  grayButton: {
-    backgroundColor: "#ccc",
-    padding: 10,
-    margin: 10,
-    borderRadius: 10,
-    color: "#000",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  heading: {
-    fontWeight: "bold",
-    textAlign: "center",
-    fontSize: 50,
-  },
-  subheading: {
-    fontWeight: "bold",
-    padding: 20,
   },
 });
