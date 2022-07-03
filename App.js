0bcc58cdad9f0d23cdc6eb4bc88d02ed42fd4fd3
jumashafara <jumashafara0@gmail.com>
2022-07-03 13:25:06 +0300

    rendering list items

--- App.js
+++ App.js
@@ -1,43 +1,34 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, TextComponent, View, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextComponent,
+  View,
+  TextInput,
+  FlatList,
+} from "react-native";
 // import { TextInput } from "react-native-web";
 
 export default function App() {
-  const [name, setName] = useState("John Doe");
-  const [age, setAge] = useState(19);
-  const [hobby, setHobby] = useState("Swimming");
+  const [fruits, setFruits] = useState([
+    { name: "mango", id: 1 },
+    { name: "pineapple", id: 2 },
+    { name: "cherries", id: 3 },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>Create Person </Text>
-      <Text>Name: </Text>
-      <TextInput
-        style={styles.input}
-        placeholder="eg John Doe"
-        onChangeText={(value) => setName(value)}
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={fruits}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
       />
-
-      <Text>Age: </Text>
-      <TextInput
-        keyboardType="numeric"
-        style={styles.input}
-        placeholder="eg 19"
-        onChangeText={(value) => setAge(value)}
-      />
-
-      <Text>Hobby: </Text>
-      <TextInput
-        style={styles.input}
-        placeholder="eg Swimming"
-        onChangeText={(value) => setHobby(value)}
-      />
-
-      <View>
-        <Text>Name: {name}</Text>
-        <Text>Age: {age}</Text>
-        <Text>Hobby: {hobby}</Text>
-      </View>
+      {/* {fruits.map((fruit) => (
+        <View>
+          <Text>{fruit.name}</Text>
+        </View>
+      ))} */}
       <StatusBar style="auto" />
     </View>
   );
@@ -50,13 +41,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 20,
     margin: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "#777",
-    padding: 2,
-    borderRadius: 5,
-    width: 100,
   },
 });
