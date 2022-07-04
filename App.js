8879765ffd3912a40a1e59c5689159528171a650
jumashafara <jumashafara0@gmail.com>
2022-07-04 21:54:16 +0300

    created id generator

--- App.js
+++ App.js
@@ -12,12 +12,35 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const createID = () => {
+    let new_id = 1;
+    todos.forEach((todo) => {
+      if (todo.id == new_id) {
+        new_id = new_id + 1;
+      }
+    });
+    return new_id;
+  };
+
+  const createTodo = (todoText) => {
+    setTodos(...{ title: todoText, id: createID() });
+  };
+
   return (
     <View style={styles.container}>
       <View>
-        <Text>ToDo</Text>
-        <TextInput />
-        <Button />
+        <View>
+          <Text>Todos</Text>
+          <View></View>
+        </View>
+        <Text>New Todo</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(value) => createTodo(value)}
+        />
+        <Button title="Add Todo" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -31,5 +54,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 20,
     margin: 20,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: "gray",
+    padding: 10,
+    marginVertical: 20,
   },
 });
