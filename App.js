8b42ac0fcb41544a21868086a7706530879a7224
jumashafara <jumashafara0@gmail.com>
2022-07-04 20:56:33 +0300

    initialized todo app

--- App.js
+++ App.js
@@ -8,35 +8,17 @@ import {
   TextInput,
   FlatList,
   TouchableOpacity,
+  Button,
 } from "react-native";
 
 export default function App() {
-  const [fruits, setFruits] = useState([
-    { name: "mango", id: 1 },
-    { name: "pineapple", id: 2 },
-    { name: "cherries", id: 3 },
-  ]);
-
-  const handlePress = (id) => {
-    setFruits((prevFruits) => {
-      return prevFruits.filter((fruit) => fruit.id !== id);
-    });
-  };
   return (
     <View style={styles.container}>
-      <FlatList
-        keyExtractor={(item) => item.id}
-        data={fruits}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={() => {
-              handlePress(item.id);
-            }}
-          >
-            <Text style={styles.listItem}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      <View>
+        <Text>ToDo</Text>
+        <TextInput />
+        <Button />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -49,10 +31,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 20,
     margin: 20,
-  },
-  listItem: {
-    padding: 10,
-    margin: 10,
-    backgroundColor: "pink",
   },
 });
